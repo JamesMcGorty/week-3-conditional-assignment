49aa198c2939c8a818a52bceb1ff53ee0b8a2aa8
James McGorty <jbmcgorty@gmail.com>
2014-03-19 13:35:45 -0400

    industry js

--- industry/js/script.js
+++ industry/js/script.js
@@ -5,3 +5,10 @@
 
 // this will calculate how many pixels and in one inch on your computer screen
 // resolution size width / screen size width * resolution height / screen height = how many how many pixles are in one inch of your screen
+
+var screenRezHeight = prompt("Please enter your screens height resolution ");
+var screenRezWidth = prompt("Please enter your screen width resolution ");
+var screenSizHeight = prompt("Please enter how many inches you screens height is ");
+var screenSizWidth = prompt("Please enter how many inches your screens width is ");
+
+var 
