b871ac9eaeba64ef78b3b92c7dda43bd1eff5aab
James McGorty <jbmcgorty@gmail.com>
2014-03-19 13:28:20 -0400

    industry js

---
+++ industry/js/script.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+//James McGorty
+//3/19/14
+//conditions industryassingment
+
+// this will calculate how many pixels and in one inch on your computer screen
+// resolution size width / screen size width * resolution height / screen height = how many how many pixles are in one inch of your screen
