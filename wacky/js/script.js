baecd6a8497cda4ed55f9a05cdcb404c9f831ba1
James McGorty <jbmcgorty@gmail.com>
2014-03-20 12:16:16 -0400

    wacky js

--- wacky/js/script.js
+++ wacky/js/script.js
@@ -7,6 +7,12 @@
 
 var gallons = prompt("How many gallons will you pump?");
 var gallonPrice = prompt("How much does a gallon cost?");
-var carWash = prompt("would you like a car wash?");
+var carWash = prompt("would you like a car wash? Yes or No");
 
+// cost of  gas for your car
+
+var gasCost = gallons * gallonPrice;
+var carWashYes = gasCost + 8;
+
+if(carWash == "yes") ? document.write("The price for your gas and wash is " + carwWashYes) : 
 
