076886ae8107efc55c89259027da9710429118ed
James McGorty <jbmcgorty@gmail.com>
2014-03-20 08:10:11 -0400

    personal js

---
+++ personal/js/script.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+//James McGorty
+//3/20/14
+//conditional personal
+
+//this calculator is to find out how much you should tip someone
+
+var bill = prompt("Please enter your the amount of your bill ");
+var tax = prompt("Please enter the tax rate ");
+var tip = prompt("Please enter how your service was, exellent, good, bad ");
+
