4bc1cc59cd55f35336254c96bd4262d0742b63b9
James McGorty <jbmcgorty@gmail.com>
2014-03-20 08:18:14 -0400

    personal js

--- personal/js/script.js
+++ personal/js/script.js
@@ -6,6 +6,11 @@
 //this calculator is to find out how much you should tip someone
 
 var bill = prompt("Please enter your the amount of your bill ");
-var tax = prompt("Please enter the tax rate ");
+var tax = prompt("Please enter the tax rate " , .075);
 var tip = prompt("Please enter how your service was, exellent, good, bad ");
 
+//sales tax is the item cost * .075
+var billWithTax = bill * tax
+
+
+// how much to tip 20% exellent 15% good 10% bad
