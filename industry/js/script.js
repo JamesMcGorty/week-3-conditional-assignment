6e29f5b53f34d94ff5633a272cb90768f967c4cb
James McGorty <jbmcgorty@gmail.com>
2014-03-20 07:59:26 -0400

    industryjs

--- industry/js/script.js
+++ industry/js/script.js
@@ -11,4 +11,8 @@ var screenRezWidth = prompt("Please enter your screen width resolution ");
 var screenSizHeight = prompt("Please enter how many inches you screens height is ");
 var screenSizWidth = prompt("Please enter how many inches your screens width is ");
 
-var 
+var height = screenRezHeight / screenSizHeight;
+var width = screenRezWidth / screenSizWidth;
+var howMany = height * width
+
+document.write("Your screen has " + howMany);
