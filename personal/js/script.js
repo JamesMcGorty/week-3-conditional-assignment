acc762541bdab534900ee9062ff1e1be30fff04f
James McGorty <jbmcgorty@gmail.com>
2014-03-20 09:00:41 -0400

    personal index

--- personal/js/script.js
+++ personal/js/script.js
@@ -11,6 +11,18 @@ var tip = prompt("Please enter how your service was, exellent, good, bad ");
 
 //sales tax is the item cost * .075
 var billWithTax = bill * tax
+var tipAdded = "please choose a selection"
 
 
 // how much to tip 20% exellent 15% good 10% bad
+
+if (tip == "exellent")
+	tipAdded = (billWithout * .2);
+else if (tip == "good")
+	tipAdded = billWithout * .15;
+else if (tip == "bad")
+tipAdded = billWithout * .1;
+
+document.write("the bill is" + bill)
+document.write("with tax added" + billWithTax)
+document.write("bill with tip" + tipAdded)
